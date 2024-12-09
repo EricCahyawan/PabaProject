86364da85f87daac4191e1aebb09c522247fe02c
EricCahyawan <137325542+EricCahyawan@users.noreply.github.com>
2024-12-09 20:12:58 +0700

    fragment container activity_main.xml

--- app/src/main/java/eric/app/pabaproject/MainActivity.kt
+++ app/src/main/java/eric/app/pabaproject/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     lateinit var indext: TextView
@@ -19,6 +20,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        if (savedInstanceState == null) {
+            val fragment = JadwalUntukPesanLapangan()
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 
 
