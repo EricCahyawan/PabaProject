d1e31e280f9fd52886c83f44a8af814a57c49285
EricCahyawan <137325542+EricCahyawan@users.noreply.github.com>
2024-12-10 00:40:04 +0700

    fragment_date_selection.xml perlu perbaikan

--- app/src/main/java/eric/app/pabaproject/dateSelection.kt
+++ app/src/main/java/eric/app/pabaproject/dateSelection.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class JadwalUntukPesanLapangan : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [dateSelection.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class dateSelection : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -26,24 +33,23 @@ class JadwalUntukPesanLapangan : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_jadwal_untuk_pesan_lapangan, container, false)
-
-        val constraintLayoutTanggal: ConstraintLayout = view.findViewById(R.id.constraintLayout)
-
-        // Listener untuk mengganti fragment tanpa commit ekstensi
-        constraintLayoutTanggal.setOnClickListener {
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainer, dateSelection())
-            transaction.addToBackStack(null) // Menambahkan ke backstack
-            transaction.commit() // Menyelesaikan transaksi
-        }
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_date_selection, container, false)
     }
 
     companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment dateSelection.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            JadwalUntukPesanLapangan().apply {
+            dateSelection().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
