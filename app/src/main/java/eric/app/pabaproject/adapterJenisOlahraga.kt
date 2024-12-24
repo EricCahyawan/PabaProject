eec147a3f86e7f69c5d02e876e34cfc16cc4e84c
bertt11 <131933705+bertt11@users.noreply.github.com>
2024-12-24 14:10:12 +0800

    update gambar

--- app/src/main/java/eric/app/pabaproject/adapterJenisOlahraga.kt
+++ app/src/main/java/eric/app/pabaproject/adapterJenisOlahraga.kt
@@ -3,8 +3,10 @@ package eric.app.pabaproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
@@ -16,12 +18,14 @@ class adapterJenisOlahraga (private val listJenisOlahraga: ArrayList<JenisOlahra
         var _gambarOlahraga = itemView.findViewById<ImageView>(R.id.gambarOlahraga)
       }
 
+    //bawaan
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view: View = LayoutInflater.from(parent.context)
             .inflate(R.layout.rv_olahraga,parent , false)
         return ListViewHolder(view)
     }
 
+    //bawaan
     override fun getItemCount(): Int {
         return listJenisOlahraga.size
     }
@@ -29,9 +33,28 @@ class adapterJenisOlahraga (private val listJenisOlahraga: ArrayList<JenisOlahra
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         var JenisOlahraga = listJenisOlahraga[position]
 
+        //penampilan nama olahraga
         holder._namaOlahraga.setText(JenisOlahraga.nama)
+        //menampilkan gambar icon olahraga
         Picasso.get()
             .load(JenisOlahraga.gambar)
             .into(holder._gambarOlahraga)
+
+        //supaya gambar bisa di click
+        holder._gambarOlahraga.setOnClickListener {
+        Toast.makeText(holder.itemView.context,JenisOlahraga.nama,Toast.LENGTH_LONG).show()
+        }
     }
+
+    //penambahan intent ketika click sesuatu
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data:JenisOlahraga)
+    }
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+
 }
